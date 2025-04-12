8d1f298f7e716cab5288736e9589ed9eed7ffde7
karthikeyan.k <k.karthikeyankcbe@gmail.com>
2025-04-12 23:09:36 +0530

    Customer Methods

--- E-Commerce/services/customer/src/main/java/com/java/customer/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.java.customer.repository;
-
-import com.java.customer.entity.Customer;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository  extends MongoRepository<Customer,String> {
-}
